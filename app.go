86912869852b0b4485c65a65e4241dcc46c37348
Ihor Adamov <adamvigr@gmail.com>
2023-08-30 19:54:17 +0300

    simple site is done

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -34,14 +35,33 @@ func getStrings(fileName string) []string {
 	check(scanner.Err())
 	return lines
 }
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	files, err := template.ParseFiles("templates/new.html")
+	check(err)
+	err = files.Execute(writer, nil)
+	check(err)
+}
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	value := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	defer file.Close()
+	check(err)
+	_, err = fmt.Fprintln(file, value)
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
 
+func deleteHandler(writer http.ResponseWriter, request *http.Request) {
+
+}
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	signatures := getStrings("signatures.txt")
 	guestbook := Guestbook{
 		SignatureCount: len(signatures),
 		Signatures:     signatures,
 	}
-	files, err := template.ParseFiles("view.html")
+	files, err := template.ParseFiles("templates/view.html")
 	check(err)
 	err = files.Execute(writer, guestbook)
 	check(err)
@@ -49,6 +69,8 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
