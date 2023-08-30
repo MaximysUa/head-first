4e93550e3150b808c43e52f56ca561f31319e2b4
Ihor Adamov <adamvigr@gmail.com>
2023-08-30 18:52:16 +0300

    initial commit

---
+++ app.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	files, err := template.ParseFiles("view.html")
+	check(err)
+	err = files.Execute(writer, guestbook)
+	check(err)
+}
+
+func main() {
+	http.HandleFunc("/guestbook", viewHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+}
