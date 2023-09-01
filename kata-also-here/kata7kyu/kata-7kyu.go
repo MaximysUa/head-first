da2c801d2a86c404a140147b2059d4e5c4c42cc0
Ihor Adamov <adamvigr@gmail.com>
2023-09-01 19:03:20 +0300

    i decided to solve kata here. why not?

---
+++ kata-also-here/kata7kyu/kata-7kyu.go
@@ -0,0 +1,60 @@
+package kata7kyu
+
+import (
+	"math"
+)
+
+func BreakChocolate(n, m int) int {
+	if n == 0 || m == 0 {
+		return 0
+	}
+	if n <= 1 && m <= 1 {
+		return 0
+	}
+
+	return (n * m) - 1
+}
+
+// How to improve my func
+// func IsLeapYear(year int) bool {
+// return year % 400 == 0 || year % 100 != 0 && year % 4 == 0
+// }
+func IsLeapYear(year int) bool {
+	if year < 1 {
+		return false
+	}
+	if year%400 == 0 {
+		return true
+	}
+	if year%4 == 0 {
+		if year%100 == 0 {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+func FunWithBinary(n, b int) []int {
+	var res []int
+	if b == 0 || n == 0 {
+		return res
+	}
+	for i := 1; i <= int(math.Pow(2.0, (float64(n)))-1); i++ {
+		if b&i > 0 {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+func PickGrains(grains <-chan string) (good int, bad int) {
+	for i := range grains {
+		if i == "good" {
+			good++
+		} else {
+			bad++
+		}
+	}
+
+	return
+}
---
+++ kata-also-here/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"head-first/kata-also-here/kata7kyu"
+)
+
+func main() {
+	fmt.Println(kata7kyu.IsLeapYear(2020))
+}
